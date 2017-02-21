1c84f9bfc1eac62f9c6d647ac1e263a7ef78bdd8
rdf014 <rdf014@gmail.com>
2017-02-20 21:48:01 -0800

    Working on SongQueueView

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,29 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+    this.on('ended', function() {
+      var toDelete = this.models[0];
+      this.remove(toDelete);
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    });
+    this.on('dequeue', function() {
+      var toDelete = this.models[0];
+      this.remove(toDelete);
+    });
+  },
+  playFirst: function() {
+    this.models[0].play();
   }
+  
+
+
+   
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,9 +2,12 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    
+
     return this.$el;
   }
 
